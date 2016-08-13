849efeff54e8392f0c3bd9d0949f21630a6d1cb8
sudhir kumar raju <mesksrk@gmail.com>
2016-08-14 02:51:20 +0530

    Create greater-of-2-number.c

---
+++ greater-of-2-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+   int a,b;
+   
+    printf("enter any two no:\n");
+    scanf("%d%d%d",&a, &b);
+      if (a>b)
+         printf("greatest of both is: %d",a);
+      else
+         printf("greatest of both is: %d",b);
+}
+   
