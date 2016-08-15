aee90000af62e5ae596f9e8c1796c08b93a8aeb6
sudhir kumar raju <mesksrk@gmail.com>
2016-08-16 01:14:49 +0530

    Create greater-of-3-number.c

---
+++ greater-of-3-number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter three number");
+  scanf("%d%d%d", &a, &b, &c);
+  
+  if(a>b)
+       if (a>c)
+       printf("greatest of the three is: %d", a);
+       else
+       printf("greatest of the three is: %d", c);
+  else if(b>c)
+  printf("greatest of the three is: %d", b);
+  else
+  printf("greatest of the three is: %d", c);
+  return 0;
+} 
